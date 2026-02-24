b03e33458d0135ce4e0b92a50de682d9d031269c
brighto7700 <brighto7700@gmail.com>
2026-02-24 05:43:46 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async redirects() {
-    return [
-      {
-        // This handles the transition from your old brand naming conventions
-        source: '/old-path-example', 
-        destination: '/', 
-        permanent: true, // This is critical for SEO to signal a permanent move
-      },
-    ];
-  },
-};
-
+const nextConfig = {};
 export default nextConfig;
